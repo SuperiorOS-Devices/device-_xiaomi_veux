1123d0739e02e8c00c9244ec3ac38018140b24c0
sourav2k <souravgope765@gmail.com>
2022-09-18 18:53:05 +0530

    Initial SuperiorOS-sdk overlays
    Signed-off-by: sourav2k <souravgope765@gmail.com>

--- device.mk
+++ device.mk
@@ -305,6 +305,9 @@ PRODUCT_PACKAGES += \
    TelephonyOverlayVeux \
    WifiOverlayVeux
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-superior
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
